38ce1f9492359e1b479e6dca6699f5d3451b61df
inkversestudio <inkversestudio.co@gmail.com>
2026-08-10 12:58:54 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,119 @@
+const CACHE_NAME = "inkverse-studio-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+
+/* =========================
+   INSTALL
+========================= */
+
+self.addEventListener(
+    "install",
+    function(event){
+
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            ).then(
+                function(cache){
+
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+
+                }
+            )
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+/* =========================
+   ACTIVATE
+========================= */
+
+self.addEventListener(
+    "activate",
+    function(event){
+
+        event.waitUntil(
+
+            caches.keys().then(
+                function(cacheNames){
+
+                    return Promise.all(
+
+                        cacheNames
+                        .filter(
+                            function(name){
+
+                                return name !==
+                                    CACHE_NAME;
+
+                            }
+                        )
+                        .map(
+                            function(name){
+
+                                return caches.delete(
+                                    name
+                                );
+
+                            }
+                        )
+
+                    );
+
+                }
+            )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+/* =========================
+   FETCH
+========================= */
+
+self.addEventListener(
+    "fetch",
+    function(event){
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            ).then(
+                function(cachedResponse){
+
+                    if(cachedResponse){
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return fetch(
+                        event.request
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);

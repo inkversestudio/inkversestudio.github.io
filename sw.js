cd8d9dd3eb3a32519acac761daecfa353057df61
inkversestudio <inkversestudio.co@gmail.com>
2026-08-12 01:37:36 +0530

    Update service worker cache configuration
    Updated cache name and added icon files to cache.

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = "inkverse-studio-v1";
+const CACHE_NAME = "inkverse-studio-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 
   self.skipWaiting();
